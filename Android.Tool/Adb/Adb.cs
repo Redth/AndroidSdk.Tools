23c480e6933b72c1f4f417101c3c58ee1bab962c
Jonathan Dick <jondick@gmail.com>
2020-01-30 21:50:48 -0500

    Actually move GetAvdName

--- Android.Tool/Adb/Adb.cs
+++ Android.Tool/Adb/Adb.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Android.Tool.Adb
@@ -492,6 +493,46 @@ namespace Android.Tool.Adb
 			Pull(new FileInfo(remoteFile), saveToLocalFile);
 
 			Shell("rm " + remoteFile);
+		}
+
+		public string GetAvdName()
+		{
+			if (!Settings.Serial.StartsWith("emulator-", StringComparison.OrdinalIgnoreCase))
+				throw new InvalidDataException("Serial must be an emulator starting with `emulator-`");
+
+			int port = 5554;
+			if (!int.TryParse(Settings.Serial.Substring(9), out port))
+				return null;
+
+			var tcpClient = new System.Net.Sockets.TcpClient("127.0.0.1", port);
+			var name = string.Empty;
+			using (var s = tcpClient.GetStream())
+			{
+
+				System.Threading.Thread.Sleep(250);
+
+				foreach (var b in Encoding.ASCII.GetBytes("avd name\r\n"))
+					s.WriteByte(b);
+
+				System.Threading.Thread.Sleep(250);
+
+				byte[] data = new byte[1024];
+				using (var memoryStream = new MemoryStream())
+				{
+					do
+					{
+						var len = s.Read(data, 0, data.Length);
+						memoryStream.Write(data, 0, len);
+					} while (s.DataAvailable);
+
+					var txt = Encoding.ASCII.GetString(memoryStream.ToArray(), 0, (int)memoryStream.Length);
+
+					var m = Regex.Match(txt, "OK(?<name>.*?)OK", RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
+					name = m?.Groups?["name"]?.Value?.Trim();
+				}
+			}
+
+			return name;
 		}
 	}
 }
