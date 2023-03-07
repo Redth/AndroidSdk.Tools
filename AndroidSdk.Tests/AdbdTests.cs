746ab7db4b42be95be47a2f7e371d95444616963
redth <jondick@gmail.com>
2023-03-07 16:27:57 -0500

    Improve tests

--- AndroidSdk.Tests/AdbdTests.cs
+++ AndroidSdk.Tests/AdbdTests.cs
@@ -1,28 +1,60 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace AndroidSdk.Tests
 {
-    public class Adbd_Tests : TestsBase
-    {
-        public Adbd_Tests(ITestOutputHelper outputHelper)
-            : base(outputHelper)
-        { }
+	public class Adbd_Tests : TestsBase
+	{
+		public Adbd_Tests(ITestOutputHelper outputHelper)
+			: base(outputHelper)
+		{ }
 
-        [Fact]
-        public async Task GetVersion()
-        {
-            var adbclient = new AdbdClient();
+		[Fact]
+		public async Task GetVersion()
+		{
+			var adbclient = new AdbdClient();
 
-            await adbclient.ConnectAsync();
+			var v = await adbclient.GetHostVersionAsync();
 
-            var v = await adbclient.GetHostVersionAsync();
-            
-            Assert.True(v != null);
-        }
-    }
+			OutputHelper.WriteLine($"Version: {v}");
+
+			Assert.True(v > 0);
+		}
+
+		[Fact]
+		public async Task GetShellProps()
+		{
+			var adbclient = new AdbdClient();
+
+			var avdName = await adbclient.GetPropAsync("emulator-5554", AdbdClient.ShellProperties.AvdName);
+			var arch = await adbclient.GetPropAsync("emulator-5554", AdbdClient.ShellProperties.ProductCpuAbi);
+
+			OutputHelper.WriteLine($"Props: {avdName} {arch}");
+
+			Assert.NotEmpty(avdName);
+		}
+
+		[Fact]
+		public async Task WatchDevices()
+		{
+			var a = new AdbdClient();
+
+			var cts = new CancellationTokenSource();
+			cts.CancelAfter(60000);
+
+			await a.WatchDevicesAsync(cts.Token, async d =>
+			{
+				OutputHelper.WriteLine($"{d.serial} -> {d.device} -> {d.state}");
+
+
+			}).ConfigureAwait(false);
+
+			
+		}
+	}
 }
