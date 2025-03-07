3e24f5a36080d0bc398f08e860a4ca80997ad2be
redth <jondick@gmail.com>
2025-03-07 16:03:00 -0500

    Add repository reader

---
+++ AndroidRepository/README.md
@@ -0,0 +1,16 @@
+﻿# Android Repository XML
+
+## Generating C# from the XSD files
+
+1. Clone https://android.googlesource.com/platform/tools/base
+2. Default branch is likely fine
+3. Collect XSD files from the following locations:
+    - `repository/src/main/resources/xsd/*.xsd`
+    - `repository/src/main/resources/xsd/sources/*.xsd`
+    - `sdklib/src/main/resources/xsd/*.xsd`
+    - `sdklib/src/main/resources/xsd/sources/*.xsd`
+    - `sdklib/src/main/resources/xsd/legacy/*.xsd`
+4. Using the `xscgen` dotnet tool (`dotnet tool install --global dotnet-xscgen`), run the command `xscgen --output="[path]" *.xsd --verbose` on all the xsd files you collected
+
+## Updating the list of URLs
+In the cloned `platform/tools/base` repo where the XSD files are found, the `sdklib/BUILD` file seems to list `generated_libs` as an array.  This looks to be roughly the actual 
---
+++ AndroidRepository/RepositoryManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace AndroidRepository;
+
+public class RepositoryManager
+{
+	readonly HttpClient httpClient;
+	private readonly Dictionary<Type, XmlSerializer> serializerCache = new();
+
+	public const string GoogleAndroidRepositoryUrlBase = "https://dl.google.com/android/repository/";
+
+	public RepositoryManager(HttpClient? httpClient = null)
+	{
+		if (this.httpClient is null)
+		{
+			this.httpClient = new HttpClient();
+			this.httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml");
+			this.httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
+			this.httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+			this.httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Charset", "ISO-8859-1");
+		}
+	}
+
+	public async Task<T> LoadUrlAsync<T>(string url)
+	{
+		var xml = await httpClient.GetStringAsync(url);
+
+		return await LoadXmlAsync<T>(xml);
+	}
+
+	public async Task<T> LoadXmlAsync<T>(string xml)
+	{
+		// Get or create the serializer for type T
+		var serializer = GetSerializer(typeof(T));
+
+		using var reader = new StringReader(xml);
+
+		// Deserialize synchronously since XmlSerializer doesn't support async operations
+		return (T)await Task.Run(() => serializer.Deserialize(reader)!);
+	}
+
+	private XmlSerializer GetSerializer(Type type)
+	{
+		if (!serializerCache.TryGetValue(type, out var serializer))
+		{
+			serializer = new XmlSerializer(type);
+			serializerCache[type] = serializer;
+		}
+		return serializer;
+	}
+
+
+	public async Task<SitesCommon_1.SiteList> GetAddonsListAsync()
+	{
+		var url = $"{GoogleAndroidRepositoryUrlBase}addons_list-6.xml";
+		var r = await LoadUrlAsync<SitesCommon_1.SiteList>(url);
+		r.Url = url;
+		return r;
+	}
+
+	public async Task<Repository2_3.SdkRepository> GetSdkRepositoryAsync()
+	{
+		var url = $"{GoogleAndroidRepositoryUrlBase}repository2-3.xml";
+		var r = await LoadUrlAsync<Repository2_3.SdkRepository>(url);
+		r.Url = url;
+		return r;
+	}
+}
